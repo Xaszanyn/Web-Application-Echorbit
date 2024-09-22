b7a33a437d18edb4db210ba185169e5f7f814299
Ekin Aslan <ekinaslan.js@gmail.com>
2024-09-22 18:03:59 +0300

    Stripe Test

--- test/index.php
+++ test/index.php
@@ -5,76 +5,57 @@ require "../utilities/configuration.php";
 
 \Stripe\Stripe::setApiKey(STRIPE_SECRET);
 
-try {
-    $session = \Stripe\Checkout\Session::create([
-        'payment_method_types' => ['card'],
-        'line_items' => [
-            [
-                'price_data' => [
-                    'currency' => 'usd',
-                    'product_data' => [
-                        'name' => 'Echorbit Audio Product Name',
-                        'description' => 'Echorbit Audio product description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                        'images' => ['https://picsum.photos/1024']
+
+if ($_GET["request"]) {
+    try {
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' => [
+                            'name' => 'Echorbit Audio Product Name',
+                            'description' => 'Echorbit Audio product description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                            'images' => ['https://picsum.photos/1024']
+                        ],
+                        'unit_amount' => 450,  // Amount in cents ($4.50)
                     ],
-                    'unit_amount' => 450,  // Amount in cents ($4.50)
-                ],
-                'quantity' => 1,
-            ]
-        ],
-        'mode' => 'payment',
-        'success_url' => 'https://echorbitaudio.com',
-        'cancel_url' => 'https://ekin.codes',
-    ]);
+                    'quantity' => 1,
+                ]
+            ],
+            'mode' => 'payment',
+            'success_url' => 'https://echorbitaudio.com',
+            'cancel_url' => 'https://ekin.codes',
+        ]);
 
-    echo $session->url;
+        header("Location: " . $session->url);
 
-} catch (\Stripe\Exception\ApiErrorException $error) {
-    echo $error->getMessage();
+    } catch (\Stripe\Exception\ApiErrorException $error) {
+        echo $error->getMessage();
+    }
 }
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-// try {
-//     $paymentIntent = \Stripe\PaymentIntent::create([
-//         'amount' => 1,
-//         'currency' => 'usd',
-//         'payment_method_types' => ['card'],
-//         'description' => 'Custom payment without product',
-//     ]);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
 
-//     echo json_encode([
-//         'client_secret' => $paymentIntent->client_secret,
-//     ]);
+<body>
+    <h1>Stripe Test Payment</h1>
+    <hr>
+    <p>Click to make payment.</p>
+    <form method="get" action="https://echorbitaudio.com/services/test/index.php">
+        <input type="hidden" name="request" value="payment">
+        <button type="submit">$4.5 Checkout</button>
+    </form>
+    <hr>
+</body>
 
-// } catch (\Stripe\Exception\ApiErrorException $error) {
-//     echo json_encode(['error' => $error->getMessage()]);
-// }
-
-
-
-
-// // Create a product
-// $product = \Stripe\Product::create([
-//     'name' => 'Sample Product',
-// ]);
-
-// // Create a price for the product
-// $price = \Stripe\Price::create([
-//     'product' => $product->id,
-//     'unit_amount' => 1000, // 1000 cents = $10
-//     'currency' => 'usd',
-// ]);
-
-// // Create a payment link
-// $paymentLink = \Stripe\PaymentLink::create([
-//     'line_items' => [
-//         [
-//             'price' => $price->id,
-//             'quantity' => 1,
-//         ],
-//     ],
-// ]);
-
-// // Output the payment link URL
-// echo "Payment URL: " . $paymentLink->url;
+</html>
