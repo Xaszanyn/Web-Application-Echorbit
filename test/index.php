8b33455abc00cddb65f68c6f4fa35b9e3b6c073e
Ekin Aslan <ekinaslan.js@gmail.com>
2024-09-22 17:53:10 +0300

    Stripe Test

--- test/index.php
+++ test/index.php
@@ -13,9 +13,11 @@ try {
                 'price_data' => [
                     'currency' => 'usd',
                     'product_data' => [
-                        'name' => 'Custom Payment Name',
+                        'name' => 'Echorbit Audio Product Name',
+                        'description' => 'Echorbit Audio product description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                        'images' => ['https://picsum.photos/512', 'https://picsum.photos/1024/512', 'https://picsum.photos/1024/2048']
                     ],
-                    'unit_amount' => 100,  // Amount in cents ($1.00)
+                    'unit_amount' => 450,  // Amount in cents ($4.50)
                 ],
                 'quantity' => 1,
             ]
