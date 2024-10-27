270c5488f62217d613cb91b3cfdc6facaa3d088f
Ekin Aslan <ekinaslan.js@gmail.com>
2024-10-27 21:23:05 +0300

    Test

--- test/index.php
+++ test/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once "../stripe/init.php";
-require_once "../stripe/init.php";
-require_once "/services/stripe/init.php";
+// require_once "../stripe/init.php";
+// require_once "../stripe/init.php";
+// require_once "/services/stripe/init.php";
 
-require "/services/utilities/database.php";
+// require "/services/utilities/database.php";
 
 
-echo "COMPLETED";
+echo $_SERVER['DOCUMENT_ROOT'];
