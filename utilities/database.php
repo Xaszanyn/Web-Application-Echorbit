e7da8e8de8e2a475b43a22d09c47be8f68b217ca
Ekin Aslan <ekinaslan.js@gmail.com>
2024-10-29 22:58:08 +0300

    Fix

--- utilities/database.php
+++ utilities/database.php
@@ -464,13 +464,6 @@ function create_order_request($session)
         'success_url' => 'https://echorbitaudio.com/store?success',
         'cancel_url' => 'https://echorbitaudio.com/store?error',
         'customer' => $customer,
-        // 'customer_email' => 'customer@example.com',
-        'payment_intent_data' => [
-            'billing_details' => [
-                'name' => 'John Doe',
-                'address' => ['country' => 'JP'],
-            ],
-        ],
     ]);
 
     $connection = connect();
