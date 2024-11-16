6e07b69941ea49e622298bd49d49734040f4ba07
Ekin Aslan <ekinaslan.js@gmail.com>
2024-11-16 20:21:35 +0300

    Test

--- test/p.php
+++ test/p.php
@@ -5,6 +5,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/services/stripe/init.php";
 
 \Stripe\Stripe::setApiKey(STRIPE_SECRET);
 
-$data = (\Stripe\Product::all())->data;
+$data = (\Stripe\Product::all(['limit' => 100]))->data;
 
 print_r(json_encode($data));
